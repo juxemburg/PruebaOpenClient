95db5793309e62c527f2a28ee4076ee957ddf22f
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2019-10-13 16:01:13 -0500

    feat(shared): se agregan módulos de scroll virtual y drag n' drop

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,11 +7,19 @@ import {
 import { fas } from '@fortawesome/free-solid-svg-icons';
 import { LoadingBarsComponent } from './components/loading-bars/loading-bars.component';
 import { LoadingSpinComponent } from './components/loading-spin/loading-spin.component';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [LoadingBarsComponent, LoadingSpinComponent],
-  imports: [CommonModule, FontAwesomeModule],
-  exports: [FontAwesomeModule, LoadingBarsComponent, LoadingSpinComponent],
+  imports: [CommonModule, FontAwesomeModule, ScrollingModule],
+  exports: [
+    FontAwesomeModule,
+    LoadingBarsComponent,
+    LoadingSpinComponent,
+    ScrollingModule,
+    DragDropModule,
+  ],
 })
 export class SharedModule {
   constructor(library: FaIconLibrary) {
