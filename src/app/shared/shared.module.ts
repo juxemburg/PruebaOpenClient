8b224a905626e950db1876c6f2d9cd7044801046
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2019-10-13 09:48:11 -0500

    feat(shared): se agregan indicadores de carga

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,16 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
 import {
   FontAwesomeModule,
   FaIconLibrary,
 } from '@fortawesome/angular-fontawesome';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { LoadingBarsComponent } from './components/loading-bars/loading-bars.component';
+import { LoadingSpinComponent } from './components/loading-spin/loading-spin.component';
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule, FontAwesomeModule, HttpClientModule],
-  exports: [FontAwesomeModule],
+  declarations: [LoadingBarsComponent, LoadingSpinComponent],
+  imports: [CommonModule, FontAwesomeModule],
+  exports: [FontAwesomeModule, LoadingBarsComponent, LoadingSpinComponent],
 })
 export class SharedModule {
   constructor(library: FaIconLibrary) {
