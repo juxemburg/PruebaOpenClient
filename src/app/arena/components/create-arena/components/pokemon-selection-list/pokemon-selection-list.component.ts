d0f1cf71abe24e1bb13b3a31183c8e7a6a23ce67
Juan Sebastian Montaño Molina <juxemburg@outlook.com>
2019-10-14 14:38:38 -0500

    feat(arena): se agregan animaciones sobre el listado de pokemones seleccionados

--- src/app/arena/components/create-arena/components/pokemon-selection-list/pokemon-selection-list.component.ts
+++ src/app/arena/components/create-arena/components/pokemon-selection-list/pokemon-selection-list.component.ts
@@ -4,11 +4,13 @@ import { CreateArenaMgrService } from '../../services/create-arena-mgr.service';
 import { Observable } from 'rxjs';
 import { PokemonShortInfoSelection } from '../../models/create-arena.models';
 import { takeUntil, map } from 'rxjs/operators';
+import { listAnimations } from 'src/assets/animations/lists.animations';
 
 @Component({
   selector: 'app-pokemon-selection-list',
   templateUrl: './pokemon-selection-list.component.html',
   styleUrls: ['./pokemon-selection-list.component.scss'],
+  animations: [listAnimations],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PokemonSelectionListComponent extends IDestroyable
